8cf1b2930ed9d27b98b63472b658c09e2010ca9c
haesooKr <gotn676@gmail.com>
2020-03-20 19:03:36 -0400

    Import messages

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -6,6 +6,7 @@ import './Chat.css';
 
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
+import Messages from '../Messages/Messages';
 
 let socket;
 
@@ -50,12 +51,11 @@ const Chat = ({ location }) => {
     }
   }
 
-  console.log(message, messages);
-
   return (
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
+        <Messages messages={messages} name={name}/>
         <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
       </div>
     </div>
